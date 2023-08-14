0309719060b0738161fb9ba1cd4d1522d3b4df4d
Josh Bax <joshbax189@gmail.com>
2023-08-15 07:55:44 +1200

    Document and tidy LSystem.hs

--- src/LSystem.hs
+++ src/LSystem.hs
@@ -1,26 +1,37 @@
 module LSystem where
 
-import Data.List.NonEmpty (NonEmpty(..))
 import qualified Data.List as List
 import Data.Maybe (fromMaybe)
 
+-- |The starting string of the L-System
 type Axiom a = [a]
+
+-- |Rules are context-free, replacing a single letter with a string.
 data Rule a = Rule {
+  -- |The letter to replace.
   tgt :: a
+  -- |The string to replace it with.
   , res :: [a]
   }
 
+-- |The collection of rules in the system. Assumes the axiom is given
+-- separately. See 'apply'.
 type Sys a = [Rule a]
 
+-- TODO unused
+-- An alternative definition of 'apply' is
+-- apply sys axiom = foldl (\acc r -> foldMap (ruleApply r) acc) axiom sys
 ruleApply :: (Eq a) => Rule a -> a -> [a]
 ruleApply (Rule c s) c'
   | c == c' = s
   | otherwise = [c']
 
+-- |Apply all rules in a system to an axiom.
+-- Assumes system is deterministic and context-free.
+--
+-- > take 3 $ iterate (apply cantor) "A"
 apply :: (Eq a) => Sys a -> Axiom a -> [a]
--- apply sys axiom = foldl (\acc r -> foldMap (ruleApply r) acc) axiom sys
-apply sys axiom = foldMap (applyAnyRule sys) axiom
-  where applyAnyRule sys c = fromMaybe [c] $ res <$> List.find ((c==) . tgt) sys 
-
--- should be able to just do
--- take 3 $ iterate (apply cantor) "A" 
+apply sys axiom = foldMap (applyAnyRule) axiom
+  -- TODO the following is a bit obscure
+  -- TODO this only works for deterministic systems, but this is not enforced
+  where applyAnyRule c = fromMaybe [c] $ res <$> List.find ((c==) . tgt) sys
